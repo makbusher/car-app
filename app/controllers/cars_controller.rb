33605df3cb70549dbb2ebdf1c9de8dff35a95162
Makayla <makaylabusher@gmail.com>
2022-12-07 19:50:44 -0600

    hard code create

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,4 +3,16 @@ class CarsController < ApplicationController
     cars = Car.all
     render json: cars.as_json
   end
+  def create
+    car = Car.new(
+    make: "Honda",
+    model: "Corolla",
+    year: "2013",
+    price: 12000,
+    color: "tan"
+    )
+    car.save
+    render json: car.as_json
+  end
+
 end
